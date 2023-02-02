a7f437b9f2342b6a9f2324f1a6afc0efd4702c1a
Vignesh <vignesh95vicke@gmail.com>
2023-02-02 15:48:08 +0530

    React prj from udemy

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ExpenseItem from './Components/ExpenseItem';
+
+function App() {
+  return (
+    <div>
+        <p>Hello</p>
+        <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import './ExpenseItem.css';
+
+function ExpenseItem() {
+    return (
+    <div className='expense-item'>
+        <div>March 4th 2022</div>
+        <div className='expense-item__description'>
+            <h2>Car Loan</h2>
+            <div className='expense-item__price'>10000</div>
+        </div>
+    </div>
+    );
+}
+
+export default ExpenseItem;
